a971cc2563bace29d56e1d6d6acf78140acd1fbd
Aditya Gupta <aditya.gupta18@imperial.ac.uk>
2019-11-12 13:44:43 +0000

    Updated version

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,23 +1,31 @@
+#pragma once
 
 #include <string>
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <bitset>
 #include "decode.hpp"
 #include "mips_instructions.hpp"
 #include "memory.hpp"
-
-
+void store_into_imem(uint32_t size_of_bin, std::string binary_num, std::vector<uint8_t> &imem){
+	for (int i = 0 ; i < size_of_bin; i++){
+		std::bitset<8> bin_i(binary_num[i]);
+		imem[i]= bin_i.to_ulong();// converts bitsets to ints and stores in mem
+	}
+}
+uint32_t pull_from_imemory(std::vector<uint8_t> imem, uint32_t pc){
+	return (imem[pc] << 24) + (imem[pc+1] << 16) + (imem[pc+2]<< 8)	+ (imem[pc+3]);
+}
 
 int main(int argc, char* argv[]){
 	
 	std::vector<uint8_t> imem;
-	imem.resize(IMEMLENGTH);
+	imem.resize(IMEM_LENGTH);
 	std::vector<uint8_t> dmem;
 	dmem.resize(0x4000000);
 	std::fill(imem.begin(), imem.end(), 0);
-	std::fill(dmem.begin(), dmem.end(), 0);	
 	uint32_t pc = IMEMOFFSET;
 	uint32_t nextpc = pc+4;
 	int32_t registers[32] = {0};
@@ -40,23 +48,24 @@ int main(int argc, char* argv[]){
 	}
 	binStream.close();
 	store_into_imem(size_of_bin, binary_num, imem);
-	while(1){
+	while(pc!=0){
 		
-		if(pc<IMEMOFFSET || pc> IMEMOFFSET + IMEMLENGTH || pc % 4 != 0){
+		if(pc<IMEMOFFSET || pc> IMEMOFFSET + IMEM_LENGTH){
 		exit(-12);
 		}
 		else{
 			for (int i = 0 ; i < size_of_bin; i++){
-				uint32_t instruction = pull_word_from_memory(imem, i);
+				uint32_t instruction = pull_from_imemory(imem, i);
 				uint32_t tmp =Instruction_decode(instruction, instruction_segments);
-				instruction_rc retcode = MIPS_instruction(registers, HI, LO, pc, nextpc, dmem, instruction_segments, imem);
+				instruction_rc retcode = MIPS_instruction(registers, HI, LO, pc, nextpc, dmem, instruction_segments);
 				std::cerr<<std::endl;
 				std::cerr<< registers[instruction_segments[1]]<< " rs" << std::endl;
 				std::cerr<< registers[instruction_segments[2]]<< " rt" << std::endl;
 				std::cerr<< registers[instruction_segments[3]]<< " rd" << std::endl;
+
 				std::cerr<< pc << "  pc "<<std::endl;
 				instruction_segments.clear();
-				std::cout<<std::endl;
+				std::cerr<<std::endl;
 				i+=3;// creates instructions from memory
 		}
 		exit(0);
